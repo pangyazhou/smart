2e3f8accadf9d495ea6db1f01fe86881d5bf29af
yazhou.pang <18856833949@163.com>
2018-12-12 11:36:52 +0800

    web构建

---
+++ src/main/java/com/ligoo/chapter1/HelloServlet.java
@@ -0,0 +1,27 @@
+package com.ligoo.chapter1;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @Author: Administrator
+ * @Date: 2018/12/12 11:20:49
+ * @Description:
+ */
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String currentTime = format.format(new Date());
+        req.setAttribute("currentTime", currentTime);
+        req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req, resp);
+    }
+}
